71f315f863f712866a6776733f177aee7923c170
kyalo460 <kyalo460@gmail.com>
2023-12-01 08:13:28 +0300

    Stopped returning 0 if node has no child

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,13 +14,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left && tree->right)
-	{
+	if (tree->left)
 		balanced += binary_tree_balance(tree->left);
+	if (tree->right)
 		balanced += binary_tree_balance(tree->right);
-	}
-	else
-		return (0);
 
 	balanced += binary_tree_balance(tree);
 	if (balanced != 0)
